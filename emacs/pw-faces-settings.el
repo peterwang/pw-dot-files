e890c01e973ed1bd772ab72b56300c4e2cb2ec8c
Peter Wang <peterwang@vip.qq.com>
2009-05-02 22:16:50 +0800

    faces in terminal.

--- emacs/pw-faces-settings.el
+++ emacs/pw-faces-settings.el
@@ -1,7 +1,14 @@
+;; customize faces of emacs in a terminal/console 
+
 (provide 'pw-faces-settings)
 
 (when (not window-system)
-  (set-face-foreground 'minibuffer-prompt "Cyan")
+  ;; cyan
+  (set-face-foreground 'minibuffer-prompt "#00ffff")
+  ;; blue3
+  (set-face-background 'region "#0000cd")
+  ;; dark cyan
+  (set-face-foreground 'comint-highlight-prompt "#008b8b")
   t)
 
 (message "faces settings loaded.")
