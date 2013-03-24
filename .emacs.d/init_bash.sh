28a3bf4a8aaac39aae0943cd8b4e092b412d2cb2
Peter Wang <ptr.wang@gmail.com>
2013-03-24 16:29:06 +0100

    color theme tweak and new configuration files.

---
+++ .emacs.d/init_bash.sh
@@ -0,0 +1,7 @@
+# for M-x shell
+
+alias ls='gls --color=auto'
+alias grep='grep --color=auto'
+export PS1="\t \W \\$ "
+
+
--- .emacs.d/pw-init.el
+++ .emacs.d/pw-init.el
@@ -15,7 +15,10 @@
 		      markdown-mode
 		      yaml-mode
 		      zenburn-theme
-		      solarized-theme
+		      graphviz-dot-mode
+		      ;solarized-theme
+		      ;color-theme-sanityinc-solarized
+		      color-theme-solarized
 		      smex
 		      session
 		      anything
@@ -40,10 +43,12 @@
 ;; load theme
 (setq custom-safe-themes
       '("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c"
+	"fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6"
 	default))
 (load-theme 'solarized-dark)
 
 ;; customize UI
+(display-time-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (column-number-mode 1)
@@ -120,11 +125,17 @@
 (set-language-environment "UTF-8")
 (fset 'yes-or-no-p 'y-or-n-p)
 
-
 (progn
   (setq frame-title-format "%b - %f")
+  (setq shell-file-name (executable-find "bash"))
+  (setq display-time-day-and-date t)
+  (setq display-time-24hr-format t)
   (setq make-backup-files nil)
   (setq dired-use-ls-dired nil)
   (setq inhibit-startup-screen t)
   (setq initial-scratch-message nil)
   (setq ring-bell-function (lambda () nil)))
+
+
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
